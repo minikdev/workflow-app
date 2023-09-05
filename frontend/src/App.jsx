3e8f6938d6d02d18dac69930fed498dcfcc25f20
Kaan Engin <enginkaan6@gmail.com>
2023-09-05 23:14:48 +0300

    initialize react query, react query devtools and reactflow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,34 @@
 import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import ReactFlow from 'reactflow';
 
+import 'reactflow/dist/style.css';
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchInterval: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <button class="btn">Button</button>
+    <QueryClientProvider client={client}>
+      <>
+      <div className='w-96 h-96 bg-slate-500'>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
       </div>
+      </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
 
-    </>
   )
 }
 
